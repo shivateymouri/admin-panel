963d242c62145ea3354f4941b1a22d43775e8162
shivateymouri <shivateymouri.st@gmail.com>
2025-06-01 21:26:45 +0330

    up

--- src/components/Container/container.jsx
+++ src/components/Container/container.jsx
@@ -12,7 +12,7 @@ export default function container() {
     <div>
       <Header />
       <div className="text-white font-sans text-xl overflow-y-hidden">
-        <div className="w-full flex flex-wrap justify-around mt-8 px-2">
+        <div className="w-full flex flex-wrap justify-around gap-5 mt-8 px-2">
           <div className="bg-[#1f1f1f] sm:w-[47%] lg:w-[23%] w-full rounded-md border border-gray-700">
             <div className="flex justify-between items-center p-5">
               <div className="flex flex-col gap-2">
